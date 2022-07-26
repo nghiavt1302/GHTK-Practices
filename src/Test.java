509a3eebceb4c006717c3bba4b569dc6f5d2533b
nghiavt1302 <vtnghia01.bchkl@gmail.com>
2022-07-26 23:20:01 +0700

    nothing

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,13 @@
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class Test {
     public static void main(String[] args) {
-        int a =10;
-        System.out.println(a);
+        long a = System.currentTimeMillis();
+        Date a1 = new Date(a);
+        System.out.println(a1);
+
+        Timestamp a2 = new Timestamp(System.currentTimeMillis());
+        System.out.println(a2);
     }
 }
